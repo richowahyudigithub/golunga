84ebfa448dd1e33cf5dd47216055086f55ff3609
richo wahyudi <57027057+devricho@users.noreply.github.com>
2021-10-29 01:47:08 +0700

    halaman home

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,26 +1,36 @@
 @extends('layouts.app_admin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Halaman admin</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Login sebagai admin!
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="jumbotron">
+  <h1 class="display-4">Hello, admin lungo!!!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 </div>
-<section></senction>
-<section></senction>
-<section></senction>
+
+<ul class="list-unstyled">
+  <li class="media">
+    <img src="..." class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p>All my girls vintage Chanel baby. So you can have your cake. Tonight, tonight, tonight, I'm walking on air. Slowly swallowing down my fear, yeah yeah. Growing fast into a bolt of lightning. So hot and heavy, 'Til dawn. That fairy tale ending with a knight in shining armor. Heavy is the head that wears the crown.</p>
+    </div>
+  </li>
+  <li class="media my-4">
+    <img src="..." class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p>Maybe a reason why all the doors are closed. Cause once you’re mine, once you’re mine. Be your teenage dream tonight. Heavy is the head that wears the crown. It's not even a holiday, nothing to celebrate. A perfect storm, perfect storm.</p>
+    </div>
+  </li>
+  <li class="media">
+    <img src="..." class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p>Are you brave enough to let me see your peacock? There’s no going back. This is the last time you say, after the last line you break. At the eh-end of it all.</p>
+    </div>
+  </li>
+</ul>
+
 @endsection
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,23 +1,79 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Halaman USER</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Login sebagai user!
-                </div>
-            </div>
-        </div>
-    </div>
+<section>
+<div class="jumbotron">
+  <h1 class="display-4">Hello, traveller</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 </div>
+<img src="https://static.misteraladin.com/images/banner/0000003000/0000002283/ojk_homepage_banner_tamasya_banyak_cara_1920x500px.jpg" class="img-fluid" alt="...">
+<hr>
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="#">
+        <img src="https://static.misteraladin.com/images/hotel/0000149000/0000148018/glamping-malabar-by-pondok-rasamala-photo6.jpg" alt="Lights" style="width:100%">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="#">
+        <img src="https://static.misteraladin.com/images/hotel/0000149000/0000148018/glamping-malabar-by-pondok-rasamala-photo6.jpg" alt="Nature" style="width:100%">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="#">
+        <img src="https://static.misteraladin.com/images/hotel/0000149000/0000148018/glamping-malabar-by-pondok-rasamala-photo6.jpg" alt="Fjords" style="width:100%">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+<ul class="list-unstyled">
+  
+  <li class="media my-4">
+    <img src="https://www.misteraladin.com/img/about/no-extra-charge.png" class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p>Maybe a reason why all the doors are closed. Cause once you’re mine, once you’re mine. Be your teenage dream tonight. Heavy is the head that wears the crown. It's not even a holiday, nothing to celebrate. A perfect storm, perfect storm.</p>
+    </div>
+  </li>
+  <li class="media">
+    <img src="https://www.misteraladin.com/img/about/easy-fast-secure-payment.png" class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p>Are you brave enough to let me see your peacock? There’s no going back. This is the last time you say, after the last line you break. At the eh-end of it all.</p>
+    </div>
+  </li>
+  <li class="media">
+    <img src="https://www.misteraladin.com/img/about/support.png" class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p>Are you brave enough to let me see your peacock? There’s no going back. This is the last time you say, after the last line you break. At the eh-end of it all.</p>
+    </div>
+  </li>
+  <li class="media">
+    <img src="https://www.misteraladin.com/img/about/amazing-deals.png" class="mr-3" alt="...">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">List-based media object</h5>
+      <p>All my girls vintage Chanel baby. So you can have your cake. Tonight, tonight, tonight, I'm walking on air. Slowly swallowing down my fear, yeah yeah. Growing fast into a bolt of lightning. So hot and heavy, 'Til dawn. That fairy tale ending with a knight in shining armor. Heavy is the head that wears the crown.</p>
+    </div>
+  </li>
+</ul>
+
+</section>
 @endsection
